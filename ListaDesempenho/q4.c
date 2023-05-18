358e37dadeb1df4264c07250a57ab499eef079f7
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-18 14:29:40 -0300

    Adição q4 da lista de desempenho

---
+++ ListaDesempenho/q4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+int is_prime(long long n){
+    long long i,divisores=0;
+    for (i=1 ; i<=n ; ++i)
+        if (n%i==0)
+            divisores++;
+    return divisores==2?1:0;
+}
+
+int main(){
+    long long n;
+    clock_t inicio,fim;
+    scanf("%lld",&n);
+    while(n!=EOF){
+        scanf("%lld",&n);
+        inicio = clock();
+        int ppp = is_prime(n);
+        fim = clock();
+        fprintf(stderr,"[%lld] Tempo: %lld milisegundos.\n",n,(long long)((fim-inicio)*1000)/CLOCKS_PER_SEC);
+        if (ppp)
+            printf("%lld é primo.\n",n);
+        else
+            printf("%lld não é primo.\n",n);
+    }
+   return 0;
+}

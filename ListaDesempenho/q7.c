71fd91ff6c979d5e4940b0551b523124e8e8d626
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-05-22 10:54:15 -0300

    Adição da questão 6 e 7 da lista de desempenho

--- ListaDesempenho/q7.c
+++ ListaDesempenho/q7.c
@@ -11,17 +11,29 @@ unsigned int retornar_menor(int array[]){
     return array[menor];
 }
 
+unsigned int in_array(int array[], int value){
+    int true = 0;
+    for(int i = 0; i < sizeof(array)+1; i++){
+        if(array[i] == value){
+            true = 1;
+            break;
+        }
+    }
+    return true;
+}
+
 int main(){
     clock_t inicio,fim;
-    int n;
+    int n, value;
     scanf("%d", &n);
     int array[n];
     for(int i = 0; i < n; i++){
         scanf("%d", &array[i]);
     }
+    scanf("%d", &value);
     inicio = clock();
-    int menor = retornar_menor(array);
+    int true = in_array(array, value);
     fim = clock();
     fprintf(stderr,"[%lld] Tempo: %lld milisegundos.\n",n,(long long)((fim-inicio)*1000)/CLOCKS_PER_SEC);
-    printf("%d", menor);
+    printf("%d", true);
 }
